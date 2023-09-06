836451635e6db6910b5f468c664d2bd1766e319b
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-06 22:49:08 +0300

    KS-1-3 Space fly dates declared and printed

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,2 +1,20 @@
 package lesson1
 
+fun main() {
+
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print(hour)
+    print(':')
+    print(minute)
+
+}
