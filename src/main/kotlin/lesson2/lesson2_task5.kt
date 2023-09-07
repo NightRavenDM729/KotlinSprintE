73ac077db1c5cb8affc48d3a55656b953ca56667
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-07 20:22:57 +0300

    KS-2-5* total money in deposit printed

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -1,2 +1,16 @@
 package lesson2
 
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
+fun main() {
+
+    val moneyInDeposit = 70000.0
+    val percentageOfDeposit = 16.7
+    val yearsOfDeposit = 20
+
+    val depositSum = moneyInDeposit * (1 + 0.01 * percentageOfDeposit).pow(yearsOfDeposit)
+    val roundedDepositSum = (depositSum * 1000).roundToInt() / 1000.0
+
+    println(roundedDepositSum)
+}
