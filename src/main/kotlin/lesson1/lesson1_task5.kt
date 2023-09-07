77fa1254dfba1463655450e2124f18f04a5fccf3
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-07 09:04:04 +0300

    KS-1-5* seconds to hh:mm:ss

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,2 +1,13 @@
 package lesson1
 
+fun main() {
+
+    val secondsTotal = 6480
+    val seconds = secondsTotal % 60
+    val minutesTotal = secondsTotal / 60
+    val minutes = minutesTotal % 60
+    val hours = minutesTotal / 60
+    val formatted = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    println(formatted)
+}
