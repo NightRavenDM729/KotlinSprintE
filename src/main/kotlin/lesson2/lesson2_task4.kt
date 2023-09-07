6035ed3fb7b1afb3fed5f53b04c3ae840a9756e7
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-07 17:37:33 +0300

    KS-2-4 buffed resources printed

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,2 +1,14 @@
 package lesson2
 
+fun main() {
+
+    val crystalOre = 7
+    val ironOre = 11
+
+    val buffCrystalOre = crystalOre / 5
+    val buffIronOre = ironOre / 5
+
+    println(buffCrystalOre)
+    println(buffIronOre)
+
+}
