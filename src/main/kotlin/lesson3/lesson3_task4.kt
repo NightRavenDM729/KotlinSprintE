9b1c02262c8c3488b187f6ea1c4d072188952066
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-09 10:36:00 +0300

    KS-3-4 chess moves declared and printed

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -1,2 +1,17 @@
 package lesson3
 
+fun main() {
+
+    var fromPosition = "E2"
+    var toPosition = "E4"
+    var moveNumber = 1
+
+    println("$fromPosition $toPosition $moveNumber")
+
+    fromPosition = "D2"
+    toPosition = "D3"
+    moveNumber = 2
+
+    println("$fromPosition $toPosition $moveNumber")
+
+}
