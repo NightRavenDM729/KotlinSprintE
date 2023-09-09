9de46f5c992836b610b9e3d0673ece04386c71d4
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-09 10:06:57 +0300

    KS-3-1 greeting messages printed

--- src/main/kotlin/lesson3/lesson3_task1.kt
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -1,2 +1,13 @@
 package lesson3
 
+fun main() {
+
+    val userName = "Gendalf"
+    var greeting = "Good morning"
+
+    println("$greeting, $userName!")
+
+    greeting = "Good evening"
+
+    println("$greeting, $userName!")
+}
