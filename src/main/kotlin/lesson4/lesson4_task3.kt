a82d530ba27c34c0e55e7a350b5ba5bd7c462fe6
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-10 18:33:37 +0300

    KS-4-3 curr conditions for beans growth compared with favorable result printed

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,2 +1,24 @@
 package lesson4
 
+fun main() {
+
+    val favorableWeatherIsSunny = true
+    val openTentStatus = true
+    val airHumidity = 20
+    val unfavorableSeason = "Winter"
+
+    val currentWeatherIsSunny: Boolean = true
+    val currentOpenTentStatus: Boolean = true
+    val currentAirHumidity: Int = 20
+    val currentSeason: String = "Winter"
+
+
+
+
+    println("Благоприятные ли условия сейчас для роста бобовых?" +
+            " ${(favorableWeatherIsSunny == currentWeatherIsSunny) &&
+            (openTentStatus == currentOpenTentStatus) &&
+            (airHumidity == currentAirHumidity) &&
+            (currentSeason != unfavorableSeason)}")
+
+}
