90b08a9c0086c5d1b553b6727d3a2a136b486b22
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-10 17:45:36 +0300

    KS-4-1 restaurant booked tables data printed

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,2 +1,13 @@
 package lesson4
 
+fun main() {
+
+    val totalTables = 13
+    val bookedTablesToday = 13
+    val bookedTablesTomorrow = 9
+
+    println("[Доступность столиков на сегодня: ${bookedTablesToday < totalTables}]\n[Доступность столиков на завтра: ${bookedTablesTomorrow < totalTables}]")
+
+
+
+}
