80a4ad17e8f36cdcf205c42971659d821f3e5832
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-11 14:12:20 +0300

    KS-4-2 "Average" vals to const vals

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,10 +1,10 @@
 package lesson4
 
-fun main() {
+const val MIN_WEIGHT_AVERAGE = 35
+const val MAX_WEIGHT_AVERAGE = 100
+const val MAX_VOLUME_AVERAGE = 100
 
-    val minWeightAverage = 35
-    val maxWeightAverage = 100
-    val maxVolumeAverage = 100
+fun main() {
 
     val cargoWeight1 = 20
     val cargoVolume1 = 80
@@ -13,8 +13,8 @@ fun main() {
     val cargoVolume2 = 100
 
     println("Груз с весом $cargoWeight1 кг и объемом $cargoVolume1 л соответствует категории 'Average': " +
-            "${(cargoWeight1 >= minWeightAverage) && (cargoWeight1 <= maxWeightAverage) && (cargoVolume1 < maxVolumeAverage)}")
+            "${(cargoWeight1 >= MIN_WEIGHT_AVERAGE) && (cargoWeight1 <= MAX_WEIGHT_AVERAGE) && (cargoVolume1 < MAX_VOLUME_AVERAGE)}")
     println("Груз с весом $cargoWeight2 кг и объемом $cargoVolume2 л соответствует категории 'Average': " +
-            "${(cargoWeight2 >= minWeightAverage) && (cargoWeight2 <= maxWeightAverage) && (cargoVolume2 < maxVolumeAverage)}")
+            "${(cargoWeight2 >= MIN_WEIGHT_AVERAGE) && (cargoWeight2 <= MAX_WEIGHT_AVERAGE) && (cargoVolume2 < MAX_VOLUME_AVERAGE)}")
 
 }
