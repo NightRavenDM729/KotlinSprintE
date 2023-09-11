7d9bb4fef6a2e7c7cec73a8f80733c7f5d12e102
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-11 14:09:28 +0300

    KS-4-1 total tables val is const, del empty strings

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,13 +1,11 @@
 package lesson4
 
+const val TOTAL_TABLES = 13
+
 fun main() {
 
-    val totalTables = 13
     val bookedTablesToday = 13
     val bookedTablesTomorrow = 9
 
-    println("[Доступность столиков на сегодня: ${bookedTablesToday < totalTables}]\n[Доступность столиков на завтра: ${bookedTablesTomorrow < totalTables}]")
-
-
-
+    println("[Доступность столиков на сегодня: ${bookedTablesToday < TOTAL_TABLES}]\n[Доступность столиков на завтра: ${bookedTablesTomorrow < TOTAL_TABLES}]")
 }
