14861179cc1b8e1e993cc6d3852e28e17c38caca
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-11 10:55:07 +0300

    KS-3-5* data from chess moves parsed with "split"

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -4,9 +4,9 @@ fun main() {
 
     val chessMoveData = "D2-D4;0"
 
-    val fromPosition = chessMoveData.subSequence(0,2)
-    val toPosition = chessMoveData.subSequence(3,5)
-    val moveNumber = chessMoveData.subSequence(6,7)
+    val fromPosition = chessMoveData.split("-", ";")[0]
+    val toPosition = chessMoveData.split("-", ";")[1]
+    val moveNumber = chessMoveData.split("-", ";")[2]
 
     println(fromPosition)
     println(toPosition)
