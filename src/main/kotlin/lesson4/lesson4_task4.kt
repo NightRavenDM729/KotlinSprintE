911d020d937a704ce205e5c43d7ef35019069903
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-11 11:22:57 +0300

    KS-4-4 exercises info depending on the day is printed

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -1,2 +1,23 @@
 package lesson4
 
+fun main() {
+
+    val dayOfTraining = 5
+
+    val armTraining = dayOfTraining % 2 != 0
+    val legTraining = dayOfTraining % 2 == 0
+    val backTraining = dayOfTraining % 2 == 0
+    val absTraining = dayOfTraining % 2 != 0
+
+
+
+    println("""
+        Упражнения для рук: $armTraining
+        Упражнения для ног: $legTraining
+        Упражнения для спины: $backTraining
+        Упражнения для пресса: $absTraining
+        
+               
+    """.trimIndent())
+
+}
