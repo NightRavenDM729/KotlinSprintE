8b4c421312726a0e2097df56e1d720344ecb686e
NightRavenDM729 <NightRavenDM@gmail.com>
2023-09-14 15:23:18 +0300

    KS-5-1 log in check solved

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,2 +1,15 @@
 package lesson5
 
+fun main() {
+
+    println("To log in solve the task below:\n7 + 4 = ?")
+
+    val usersAnswerToLogIn = readln().toInt()
+    val rightAnswerToLogIn = 11
+
+    if (usersAnswerToLogIn == rightAnswerToLogIn) {
+        println("Successfully! Welcome!")
+    } else {
+        println("Unsuccessful! Try again, please!")
+    }
+}
